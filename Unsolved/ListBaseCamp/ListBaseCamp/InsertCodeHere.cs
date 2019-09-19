140786356e593d0ce545ba766597683784d5de3d
HappyHeirloom <jonasgross@hotmail.com>
2019-09-19 10:09:37 +0200

    2.8 list
    Done

--- Unsolved/ListBaseCamp/ListBaseCamp/InsertCodeHere.cs
+++ Unsolved/ListBaseCamp/ListBaseCamp/InsertCodeHere.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 // ReSharper disable CollectionNeverQueried.Local
 
 namespace ListBaseCamp
@@ -16,21 +17,25 @@ namespace ListBaseCamp
             listOfInt.Add(9);
 
             // Case 1
-            // Console.WriteLine($"Case 1: Element with index 1 is {listOfInt[1]}");
+            Console.WriteLine($"Case 1: Element with index 1 is {listOfInt[1]}");
+            Console.WriteLine();
 
             // Case 2
-            // Console.WriteLine($"Case 2: List contains {listOfInt.Count} elements");
+            Console.WriteLine($"Case 2: List contains {listOfInt.Count} elements");
+            Console.WriteLine();
 
             listOfInt.Add(5);
             listOfInt.Add(22);
 
             // Case 3
-            // Console.WriteLine($"Case 3: Element with index 3 is {listOfInt[3]}");
+            Console.WriteLine($"Case 3: Element with index 3 is {listOfInt[3]}");
+            Console.WriteLine();
 
             listOfInt.RemoveAt(0);
 
             // Case 4
-            // Console.WriteLine($"Case 4: Element with index 3 is {listOfInt[3]}");
+            Console.WriteLine($"Case 4: Element with index 3 is {listOfInt[3]}");
+            Console.WriteLine();
 
 
 
@@ -43,12 +48,25 @@ namespace ListBaseCamp
             listOfInt.Add(3);
 
             // Case 5: Add code that prints out 
-            // all the elements in the list
+            //all the elements in the list
+            for (int i = 0; i < listOfInt.Count; i++)
+            {
+                Console.WriteLine(listOfInt[i]);
+            }
+            Console.WriteLine();
 
 
 
             // Case 6: Add code that finds the 
             // sum of the elements in the list, and prints the result
+            int sum = 0;
+
+            for (int i = 0; i < listOfInt.Count; i++)
+            {
+                sum = sum + listOfInt[i];
+                Console.WriteLine(sum);
+            }
+            Console.WriteLine();
 
 
 
@@ -56,12 +74,19 @@ namespace ListBaseCamp
             // Case 7: Add code that finds the smallest  
             // element in the list, and prints the result
             // Tip: Think in detail about how you would do this manually
+            Console.WriteLine(listOfInt.Min());
+            Console.WriteLine();
 
 
 
             // [(maybe) DIFFICULT]
             // Case 8: Add code that sorts the content of the list.
             // Tip: Think before you code...
+            listOfInt.Sort();
+            for (int i = 0; i < listOfInt.Count; i++)
+            {
+                Console.WriteLine(listOfInt[i]);
+            }
 
 
             // The LAST line of code should be ABOVE this line
