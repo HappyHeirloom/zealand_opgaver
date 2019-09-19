21f6c10e3bc1c2b8f94196bfa3153601dd927d4f
HappyHeirloom <jonasgross@hotmail.com>
2019-09-19 11:47:05 +0200

    2.9 roleplay
    Done med ekstra

--- Unsolved/RolePlayV21/RolePlayV21/InsertCodeHere.cs
+++ Unsolved/RolePlayV21/RolePlayV21/InsertCodeHere.cs
@@ -44,38 +44,52 @@ namespace RolePlayV21
 
             // New battle logic (1-on-many)
             #region 1-on-many battle logic
-            Hero hero = new Hero(generator, log, "stupid", 200, 20, 100);
+            int heroWon = 0;
+            int armyWon = 0;
+
+            Hero hero = new Hero(generator, log, "stupid", 200, 20, 30);
             BeastArmy theArmy = new BeastArmy();
-            Beast beast1 = new Beast(generator, log, "idiot", 70, 5, 10);
-            Beast beast2 = new Beast(generator, log, "idiot2", 70, 5, 10);
+            Beast beast1 = new Beast(generator, log, "idiot", 100, 5, 10);
+            Beast beast2 = new Beast(generator, log, "idiot2", 20, 5, 10);
+            Beast beast3 = new Beast(generator, log, "idiot3", 80, 5, 10);
+            Beast beast4 = new Beast(generator, log, "idiot4", 300, 10, 10);
 
             theArmy.AddBeast(beast1);
             theArmy.AddBeast(beast2);
+            theArmy.AddBeast(beast3);
+            theArmy.AddBeast(beast4);
 
 
-            while (!hero.Dead && !theArmy.Dead)
+            for (int i = 0; i < 100; i++)
             {
-                int heroDamage = hero.DealDamage();
-                theArmy.ReceiveDamage(heroDamage);
-
-                if (!theArmy.Dead)
+                while (!hero.Dead && !theArmy.Dead)
                 {
-                    int armyDamage = theArmy.DealDamage();
-                    hero.ReceiveDamage(armyDamage);
+                    int heroDamage = hero.DealDamage();
+                    theArmy.ReceiveDamage(heroDamage);
+
+                    if (!theArmy.Dead)
+                    {
+                        int armyDamage = theArmy.DealDamage();
+                        hero.ReceiveDamage(armyDamage);
+                    }
                 }
-            }
 
-            log.PrintLog();
-            Console.WriteLine();
-            if (theArmy.Dead)
-            {
-                Console.WriteLine($"The Hero {hero.Name} was Victorious!!");
-            }
-            else
-            {
-                Console.WriteLine($"The Beast {theArmy} won... ;-(");
-            }
 
+                log.PrintLog();
+                Console.WriteLine();
+                if (theArmy.Dead)
+                {
+                    Console.WriteLine($"The Hero {hero.Name} was Victorious!!");
+                    ++heroWon;
+                }
+                else
+                {
+                    Console.WriteLine($"The Beast army won... ;-(");
+                    ++armyWon;
+                }
+                Console.WriteLine($"The Hero was won: {heroWon} times");
+                Console.WriteLine($"The army has won: {armyWon} times");
+            }
             #endregion
 
             // The LAST line of code should be ABOVE this line
