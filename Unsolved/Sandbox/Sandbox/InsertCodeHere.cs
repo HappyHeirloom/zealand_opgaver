28854927f88fc57cdba99091688ba4b8ad56afaf
HappyHeirloom <jonasgross@hotmail.com>
2019-10-02 12:58:58 +0200

    prøve bestået/ikke bestået

--- Unsolved/Sandbox/Sandbox/InsertCodeHere.cs
+++ Unsolved/Sandbox/Sandbox/InsertCodeHere.cs
@@ -7,8 +7,28 @@ namespace Sandbox
         public void MyCode()
         {
             // The FIRST line of code should be BELOW this line
+            string name;
+            int age;
+            int number;
 
-            Console.WriteLine("Hello world!");
+            Console.WriteLine("Please tell me your name");
+            name = Console.ReadLine();
+            Console.WriteLine("Please tell me your age in numbers");
+            age = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Please tell me your phonenumber in numbers");
+            number = Int32.Parse(Console.ReadLine());
+
+
+
+            Person person1 = new Person("Jonas", 20, 21562886);
+            Person person2 = new Person("Alex", 20, 30860306);
+            Person user = new Person(name, age, number);
+
+            person1.PrintAll();
+            person2.PrintAll();
+            user.PrintAll();
+
+            person1.increaseAge();
 
             // The LAST line of code should be ABOVE this line
         }
---
+++ Unsolved/Sandbox/Sandbox/Person.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+public class Person
+{
+    #region Instance field
+    string _name;
+    int _age;
+    int _TelephoneNumber;
+    #endregion
+
+    #region Constructor
+    public Person(string name, int age, int number)
+    {
+        _name = name;
+        _age = age;
+        _TelephoneNumber = number;
+    }
+    #endregion
+
+    #region Properties
+    public string Name
+    {
+        get { return _name; }
+        set { _name = value; }
+    }
+
+    public int Age
+    {
+        get { return _age; }
+        set { _age = value; }
+    }
+
+    public int TelephoneNumber
+    {
+        get { return _TelephoneNumber; }
+        set { _TelephoneNumber = value; }
+    }
+    #endregion
+
+    #region Methods
+    public void increaseAge()
+    {
+        _age++;
+        Console.WriteLine($"\n{_name} congratulations you're now {_age} years old");
+    }
+
+    public void PrintAll()
+    {
+       Console.WriteLine($"\nHi {_name}, you are {_age} year old right? Can i give you a call on {_TelephoneNumber}");
+    }
+
+    #endregion
+
+}
