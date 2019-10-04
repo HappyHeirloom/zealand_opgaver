e363987c602a4faa11872937c8400669289f6866
HappyHeirloom <jonasgross@hotmail.com>
2019-10-04 11:28:05 +0200

    Done

--- Unsolved/RolePlayV23/RolePlayV23/Defender.cs
+++ Unsolved/RolePlayV23/RolePlayV23/Defender.cs
@@ -6,11 +6,21 @@ using System.Threading.Tasks;
 
 namespace RolePlayV23
 {
-    public class Defender
+    public class Defender : Character
     {
-        public Defender(string name, int hitPoints, int minDamage, int maxDamage)
+        public Defender(string name, int hitPoints, int minDamage, int maxDamage) : base(name, hitPoints, minDamage, maxDamage)
         {
 
+        }
+
+        protected override int ReceiveDamageModifyChance
+        {
+            get { return 45; }
+        }
+
+        protected override int CalculateModifiedReceivedDamage(int receivedDamage)
+        {
+            return (receivedDamage * 50) / 100;
         }
 
     }
--- Unsolved/RolePlayV23/RolePlayV23/InsertCodeHere.cs
+++ Unsolved/RolePlayV23/RolePlayV23/InsertCodeHere.cs
@@ -9,10 +9,14 @@
             CharacterGroup redTeam = new CharacterGroup("Team Red");
             redTeam.AddCharacter(new Character("Angor", 100, 15, 25));
             redTeam.AddCharacter(new Character("Zurin", 85, 18, 30));
+            redTeam.AddCharacter(new Defender("DefenderRed", 200, 5, 10));
+            redTeam.AddCharacter(new Damager("DamagerRed", 100, 20, 20));
 
             CharacterGroup greenTeam = new CharacterGroup("Team Green");
             greenTeam.AddCharacter(new Character("Baldur", 120, 12, 18));
             greenTeam.AddCharacter(new Character("Eliza", 80, 20, 35));
+            greenTeam.AddCharacter(new Defender("DefenderGreen", 200, 5, 10));
+            greenTeam.AddCharacter(new Damager("DamagerGreen", 100, 20, 20));
 
             BattleHandler.DoBattle(redTeam, greenTeam);
 
